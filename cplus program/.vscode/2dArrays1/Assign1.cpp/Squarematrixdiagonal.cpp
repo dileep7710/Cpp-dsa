0e05faa15ac94e1e3757a78723f80abe2c9e1e7f
dileep yadav <dy695354612@gmail.com>
2025-11-13 10:12:54 +0530

    pw

---
+++ cplus program/.vscode/2dArrays1/Assign1.cpp/Squarematrixdiagonal.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the no of row/col:-";
+    cin>>n;
+    int a[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+
+        }
+
+    }
+    cout<<endl;
+    // print
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a[i][j]<<" ";
+         }
+         cout<<endl;
+    }
+    cout<<endl;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+         if((i==j) or (i+j==n-1))
+          cout<<a[i][j]<<" ";
+          else cout<<" ";
+        }
+         cout<<endl;
+    }
+    
+
+}
