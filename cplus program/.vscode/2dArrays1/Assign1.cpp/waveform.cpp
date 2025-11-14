6a29dfbcb4f277fab6c1f128db9a65a3532b8223
dileep yadav <dy695354612@gmail.com>
2025-11-14 11:37:02 +0530

    raghav

--- cplus program/.vscode/2dArrays1/Assign1.cpp/waveform.cpp
+++ cplus program/.vscode/2dArrays1/Assign1.cpp/waveform.cpp
@@ -8,7 +8,7 @@ int main(){
         for(int j=0;j<n;j++){
             cin>>a[i][j];
         }
-    }
+    } 
     for(int  i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cout<<a[i][j]<<" ";
