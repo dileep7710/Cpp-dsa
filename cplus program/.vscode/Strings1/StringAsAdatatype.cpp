e20f14df3b89da8772df1dd4300567ef05dfcda3
dileep yadav <dy695354612@gmail.com>
2025-12-01 11:42:18 +0530

    string

---
+++ cplus program/.vscode/Strings1/StringAsAdatatype.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    // string str = "Dileep yadav";
+    // cout<<str;
+    string name;
+//     cin>>s;
+//     cout<<s;
+    //  int a,b;
+    //  cin>>a;
+    //  cin>>b;
+    //  cout<<a<<" "<<b;
+    getline(cin,name); // vvimp
+    cout<<name;
+ }
