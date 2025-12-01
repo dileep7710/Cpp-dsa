d378d78643310aab55b348b5669ffde717796c9e
dileep yadav <dy695354612@gmail.com>
2025-12-01 14:17:20 +0530

    str

--- cplus program/.vscode/Strings1/updationOfsingleChar.cpp
+++ cplus program/.vscode/Strings1/updationOfsingleChar.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main(){
     string str = "dileep";
     cout<<str<<endl;
-    str[5]='f';
+   //kaam
+   for(int i=0;str[i]!='\0';i++){
+    if(i%2==0) str[i] = 'a';
+   }
     cout<<str<<endl;
 }
