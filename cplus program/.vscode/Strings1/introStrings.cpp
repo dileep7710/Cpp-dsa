34329ccbba852ee8d46aa4a4eee307ce3cd7f042
dileep yadav <dy695354612@gmail.com>
2025-12-01 11:19:41 +0530

    string

--- cplus program/.vscode/Strings1/introStrings.cpp
+++ cplus program/.vscode/Strings1/introStrings.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    char str[5] = {'a','b','c','d','e'};
+    // char str[5] = {'a','b','c','d','e'};
+    // for(int i=0;i<5;i++){
+    //     cout<<str[i]<<" ";
+    char str[5] = "abcd";
     for(int i=0;i<5;i++){
         cout<<str[i]<<" ";
     }
-    //cout<<str;
 }
+    //cout<<str;
