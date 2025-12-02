f1f9e4abf472ac9ba4e57a8a775e34972b8e2fbe
dileep yadav <dy695354612@gmail.com>
2025-12-02 11:24:02 +0530

    dileep

---
+++ cplus program/.vscode/Strings1/builtInFunction.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+//     string str = "Raghav garg is at pw";
+//    cout<<str<<endl;
+//    str[1] = 'o';
+//     cout<<str<<endl; 
+string str = "acbd";
+cout<<str<<endl;
+str.push_back('e');
+cout<<str<<endl;
+}
