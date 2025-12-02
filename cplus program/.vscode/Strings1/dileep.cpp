0c47c831c27c95600ed4410dfba7300897feb7c4
dileep yadav <dy695354612@gmail.com>
2025-12-02 11:51:48 +0530

    str

---
+++ cplus program/.vscode/Strings1/dileep.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    char str[n];
+    for(int i=0;i<n;i++){
+        cin>>str[i];
+    }
+    int count = 0;
+    for(int i=0;i<str[i]!='\0';i++){
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+        count++;
+        }
+        cout<<count<<" ";
+}
