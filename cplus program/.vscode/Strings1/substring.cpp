bb48bad3a9baf1eb933e7cffaa55a70e3f1c2a2d
dileep yadav <dy695354612@gmail.com>
2025-12-03 10:17:45 +0530

    input str

--- cplus program/.vscode/Strings1/substring.cpp
+++ cplus program/.vscode/Strings1/substring.cpp
@@ -2,7 +2,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string s = "Dileep";
-    // s.substr(idx,len)
-    cout<<s.substr(2,3); // lee
+    // string s = "Dileep";
+    // if()
+    // // s.substr(idx,len)
+    // cout<<s.substr(2,3); // lee
+
+    string str;
+    cout<<"Enter a string :";
+    cin>>str;
+    //abcdefgh -> n=8
+    int n = str.length();
+    cout<<str.substr(n/2);
 }
