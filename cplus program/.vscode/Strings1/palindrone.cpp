0a4cb3d8cec86c2af88ec4f059c97e0f09921c96
dileep yadav <dy695354612@gmail.com>
2025-12-05 09:17:53 +0530

    palindrome

---
+++ cplus program/.vscode/Strings1/palindrone.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool isPalindrome(string s){
+    int n = s.size();
+    for(int i=0; i < n/2; i++){
+        if(s[i] != s[n-i-1]) return false;
+    }
+    return true;
+}
+
+int main(){
+    string s;
+    cin>>s;
+
+    isPalindrome(s) ? cout<<"yes, a palindrome" : cout<<"no, not a palindrome";
+
+    return 0;
+}
