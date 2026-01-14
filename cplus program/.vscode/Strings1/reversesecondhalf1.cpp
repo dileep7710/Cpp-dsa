86b43e1d6b642a79f0771a2941528248fb8b644f
dileep yadav <dy695354612@gmail.com>
2026-01-14 16:19:29 +0530

    secondhalfreverse

---
+++ cplus program/.vscode/Strings1/reversesecondhalf1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    int n = s.length();
+    reverse(s.begin()+n/2,s.end()); // inbuilt reverse function
+    cout<<s;
+   
+}  
