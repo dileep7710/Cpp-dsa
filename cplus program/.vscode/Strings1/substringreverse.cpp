52a29e6b8730950576088b4fa335eb9bb759f44e
dileep yadav <dy695354612@gmail.com>
2026-01-14 10:55:39 +0530

    substring

---
+++ cplus program/.vscode/Strings1/substringreverse.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // string s = "Dileep";
+    // cout<<s<<" ";
+    // int n = s.length();
+    // // s.substr(idx,len)
+    // cout<<s.substr(n/2)<<" ";
+    int x = 123456;
+    string s = to_string(x);
+    cout<<s.length();
+}
