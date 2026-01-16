b3bf7da457a97093e5958d2b79e996eed76ecd60
dileep yadav <dy695354612@gmail.com>
2026-01-16 17:20:19 +0530

    maxcount

--- cplus program/.vscode/string2.cpp/mostoccuringword.cpp
+++ cplus program/.vscode/string2.cpp/mostoccuringword.cpp
@@ -12,14 +12,30 @@ int main(){
     while(ss>>temp){
         v.push_back(temp);
     }
-    // vector print 
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<endl;
-    }
-    cout<<endl;
+    // // vector print 
+    // for(int i=0;i<v.size();i++){
+    //     cout<<v[i]<<endl;
+    // }
+    // cout<<endl;
+    // sort(v.begin(),v.end());
+    // // vector print 
+    // for(int i=0;i<v.size();i++){
+    //     cout<<v[i]<<endl;
+    // }
     sort(v.begin(),v.end());
-    // vector print 
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<endl;
+    int maxCount = 1;
+    int count = 1;
+    for(int i=1;i<v.size();i++){
+        if(v[i]==v[i-1]) count++;
+        else count = 1;
+        maxCount = max(maxCount,count);
+    }
+    count =  1;
+    for(int i=1;i<v.size();i++){
+        if(v[i]==v[i-1]) count ++;
+        else count = 1;
+        if(count==maxCount){
+            count<<v[i]<<" "<<maxCount<<endl;
+        }
     }
 }
