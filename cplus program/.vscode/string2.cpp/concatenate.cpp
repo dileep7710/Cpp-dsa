30625ebadf7108b660d359d2e2c0d63e121d5690
dileep yadav <dy695354612@gmail.com>
2026-01-18 15:38:55 +0530

    cocatenate

---
+++ cplus program/.vscode/string2.cpp/concatenate.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    string temp = s;
+    reverse(temp.begin(),temp.end());
+    s = s+temp;
+    cout<<s;
+}
