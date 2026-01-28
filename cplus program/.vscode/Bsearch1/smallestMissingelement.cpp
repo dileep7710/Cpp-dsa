fd4f25ac0f7fa7f78e1867bb00cd1788fe20b7e0
dileep yadav <dy695354612@gmail.com>
2026-01-28 14:55:14 +0530

    smallestMissingelement

--- cplus program/.vscode/Bsearch1/smallestMissingelement.cpp
+++ cplus program/.vscode/Bsearch1/smallestMissingelement.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {0,1,2,3, 4,8,9,12};
+    int arr[] = {0,1,2,3,4,8,9,12};
     int n = 7;
     // linear appraoch
     // for(int i=0;i<n;i++){
