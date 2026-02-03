649b137772786802cf9c441800298e1705163eac
dileep yadav <dy695354612@gmail.com>
2026-02-03 16:14:20 +0530

    javaScript

---
+++ .vscode/JavaScript/app.js
@@ -0,0 +1,33 @@
+// let a = 10;
+// let b = 5;
+// let output = "The total price is :" + ( pencilPrice + erasorPrice) + " Rupees.";
+// let output = `The total price is : ${pencilPrice} rupees.`;
+// console.log(output);
+// console.log(`Your pay ${a + b} rupees`);
+
+// let size = 'M';
+// if(size==='XL'){
+//     console.log("price is Rs:-",250);
+// }
+// else if(size==='L'){
+//     console.log("price is:-",200);
+// }
+// else if(size=='M'){
+//     console.log("price is:-",100);
+// }
+// else{
+//     console.log("price is:-",50);
+// }
+// let str = "Abhishek";
+// if((str[0]=='A') && (str.length>3)){
+//     console.log("Good");
+// }
+// else
+//     console.log("no");
+let num = 12;
+if((num%3 === 0) && ((num+1 == 15) || (num-1 == 11))){
+    console.log("safe");
+}
+else{
+    console.log("unsafe");
+}
