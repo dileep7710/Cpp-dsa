565852b7a818c4d3ea5d24f532f425fa61dce3f6
dileep yadav <dy695354612@gmail.com>
2026-02-05 11:16:53 +0530

    jsPart2

---
+++ .vscode/JavaScript/jsPart2.js
@@ -0,0 +1,37 @@
+// let x = 1010;
+// if(x%10==0)
+//     console.log("good");
+// else 
+//     console.log("bad");
+
+// let name = prompt("please enter your name");
+// let age = prompt("Please enter your Age");
+// alert(`${name} is ${age} years old`);
+
+// let Quarter = 3;
+
+// switch(Quarter){
+//     case 1:
+//         console.log("January,February,March");
+//         break;
+
+//     case 2:
+//         console.log("April,May, June");
+//         break;
+
+//      case 3:
+//         console.log("July,August,September");
+//         break;
+
+//      case 4:
+//         console.log("October,November,December");
+//         break;
+// }
+
+let str = "apple";
+
+if((str[0]=='A' || str[0]=='a') && (str.length>5)){
+    console.log("Golden");
+}
+else 
+    console.log("not");
