8c45695aa70bee97dd2ca0c499fa281a54286311
dileep yadav <dy695354612@gmail.com>
2026-02-13 01:30:39 +0530

    js

--- .vscode/JavaScript/app.js
+++ .vscode/JavaScript/app.js
@@ -195,10 +195,16 @@
 // }
 // printinfo("Dileep yadav" ,23);
 
-function sum(a,b) {
-        console.log(a+b);
-}
+// function sum(a,b) {
+//         console.log(a+b);
+// }
 
-sum(1,2);
-sum(4,5);
-sum(7,8);
+// sum(1,2);
+// sum(4,5);
+// sum(7,8);
+
+function calcAvg(a,b,c){
+        let avg = (a+b+c)/3;
+        console.log(avg);
+}
+calcAvg(2, 4, 6); 
