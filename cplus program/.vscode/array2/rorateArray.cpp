72192e806905f05188b213424645a523e6b52370
dileep yadav <dy695354612@gmail.com>
2026-02-14 13:56:33 +0530

    array

--- cplus program/.vscode/array2/rorateArray.cpp
+++ cplus program/.vscode/array2/rorateArray.cpp
@@ -39,22 +39,22 @@
 
 // }
 
-#include<iostream>
-#include<climits>
-using namespace std;
-int main(){
-    int n;
-    cout<<"Enter size of array:-";
-    cin>>n;
-    int arr[n];
-    // input
-    for(int i=0;i<=n-1;i++){
-        cin>>arr[i];
-    }
-    int max = INT_MIN;
-    cout<<"Maximum value: ";
-    for(int i=0;i<=n-1;i++){
-        if(max < arr[i]) max = arr[i];
-    }
-    cout<<max;
-}
+// #include<iostream>
+// #include<climits>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter size of array:-";
+//     cin>>n;
+//     int arr[n];
+//     // input
+//     for(int i=0;i<=n-1;i++){
+//         cin>>arr[i];
+//     }
+//     int max = INT_MIN;
+//     cout<<"Maximum value: ";
+//     for(int i=0;i<=n-1;i++){
+//         if(max < arr[i]) max = arr[i];
+//     }
+//     cout<<max;
+// }
