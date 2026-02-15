28ce558a03dcb4e5a3e9ab5baac6e7a570115166
dileep yadav <dy695354612@gmail.com>
2026-02-15 22:34:52 +0530

    function jave script

--- .vscode/JavaScript/app.js
+++ .vscode/JavaScript/app.js
@@ -227,12 +227,23 @@
 // let s= isAdult(17);
 //  console.log(s); 
 
-function getSum(n){
-    let sum = 0;
-    for(let i=1; i<=n;i++){
-        sum+=i;
+// function getSum(n){
+//     let sum = 0;
+//     for(let i=1; i<=n;i++){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+// let s = getSum(10);
+// console.log(s);
+
+let str = ["hi","hello","bye","!"];
+
+function concat(str) {
+    let result = " ";
+
+    for(let i=0;i<str.length;i++){
+        result += str[i];
     }
-    return sum;
+    return result;
 }
-let s = getSum(10);
-console.log(s);
