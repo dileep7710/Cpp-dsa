b7f204fb43e9040849db23d843d694f0ff9e78cd
dileep yadav <dy695354612@gmail.com>
2026-02-19 15:06:12 +0530

    recursion

--- cplus program/.vscode/Recursion1/functionCall.cpp
+++ cplus program/.vscode/Recursion1/functionCall.cpp
@@ -52,17 +52,30 @@
 //     saca(1,n);
 // }
 
+// #include<iostream>
+// using namespace std;
+// int fact(int n){
+//     // base case
+//     if(n==1 || n==0) return 1;
+//     // kaam and call
+//     int ans = n*fact(n-1);
+//     // return
+//     return ans;
+// }
+// int main(){
+//     cout<<fact(5);
+
+// }
 #include<iostream>
 using namespace std;
-int fact(int n){
-    // base case
-    if(n==1 || n==0) return 1;
-    // kaam and call
-    int ans = n*fact(n-1);
-    // return
-    return ans;
+void sum1toN(int sum, int n){
+    if(n==0){
+        cout<<sum<<endl;
+        return;
+    }
+    sum1toN(sum+n,n-1);
+
 }
 int main(){
-    cout<<fact(5);
-
+    sum1toN(0,5);
 }
