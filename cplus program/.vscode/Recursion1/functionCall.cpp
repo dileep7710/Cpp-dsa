9252a65397960cfb70af0cb75886eff37e3449a4
dileep yadav <dy695354612@gmail.com>
2026-02-19 15:36:41 +0530

    recursion

--- cplus program/.vscode/Recursion1/functionCall.cpp
+++ cplus program/.vscode/Recursion1/functionCall.cpp
@@ -66,16 +66,26 @@
 //     cout<<fact(5);
 
 // }
+// #include<iostream>
+// using namespace std;
+// void sum1toN(int sum, int n){
+//     if(n==0){
+//         cout<<sum<<endl;
+//         return;
+//     }
+//     sum1toN(sum+n,n-1);
+
+// }
+// int main(){
+//     sum1toN(0,5);
+// }
+
 #include<iostream>
 using namespace std;
-void sum1toN(int sum, int n){
-    if(n==0){
-        cout<<sum<<endl;
-        return;
-    }
-    sum1toN(sum+n,n-1);
-
+int sum(int n){
+    if(n==0) return 0;
+    return n+sum(n-1);
 }
 int main(){
-    sum1toN(0,5);
+    cout<<sum(5);
 }
