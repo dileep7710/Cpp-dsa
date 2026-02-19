5c4b69a54a4f6632b1ec8e6305d0b22dfcc68fc7
dileep yadav <dy695354612@gmail.com>
2026-02-19 14:27:14 +0530

    recursion

--- cplus program/.vscode/Recursion1/functionCall.cpp
+++ cplus program/.vscode/Recursion1/functionCall.cpp
@@ -1,12 +1,40 @@
+// #include<iostream>
+// using namespace  std;
+// void fun(int n){
+//     if(n==0) return;
+//     cout<<"Hello pw"<<endl;
+//     fun(n-1);
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     fun(n);
+// }
+
+// #include<iostream>
+// using namespace std;
+// void gun(int n){
+//     if(n==0) return;
+//     cout<<"Raghav Sir"<<endl;
+//     gun(n-1);
+// }
+// int main(){
+//     gun(3);
+// }
+
+//
+
 #include<iostream>
-using namespace  std;
+using namespace std;
 void fun(int n){
-    if(n==0) return;
-    cout<<"Hello pw"<<endl;
-    fun(n-1);
+    if(n==0) return; // base case
+    cout<<n<<endl; // kaam
+    fun(n-1); // call
 }
 int main(){
     int n;
     cin>>n;
+    cout<<endl;
     fun(n);
 }
+
