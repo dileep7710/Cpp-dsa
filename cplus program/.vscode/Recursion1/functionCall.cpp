06ba7e6cd7cfa4cf60a9171e73fdfd140584c953
dileep yadav <dy695354612@gmail.com>
2026-02-20 09:19:16 +0530

    recursion

--- cplus program/.vscode/Recursion1/functionCall.cpp
+++ cplus program/.vscode/Recursion1/functionCall.cpp
@@ -80,12 +80,46 @@
 //     sum1toN(0,5);
 // }
 
+// #include<iostream>
+// using namespace std;
+// int sum(int n){
+//     if(n==0) return 0;
+//     return n+sum(n-1);
+// }
+// int main(){
+//     cout<<sum(5);
+// }
+
+
 #include<iostream>
 using namespace std;
-int sum(int n){
-    if(n==0) return 0;
-    return n+sum(n-1);
+// int power(int a , int b){
+//     if(a==0 && b==0){
+//         cout<<"0 raised to the power 0 is not defined";
+//         return -100;
+//     }
+//     if(a==0) return 0;
+//      int p = 1;
+//     for(int i=1;i<=b;i++){
+//         p*=a;
+//     }
+//     return p;
+    
+// }
+
+int pow(int a, int b){
+    if(b==0) return 1;
+    return a*pow(a,b-1);
 }
 int main(){
-    cout<<sum(5);
+    int a;
+    cout<<"enter base: ";
+    cin>>a;
+
+    int b;
+    cout<<"enter exponent :";
+    cin>>b;
+
+
+    cout<<a<<" raised to the power "<<b<< " is "<<pow(a,b);
 }
