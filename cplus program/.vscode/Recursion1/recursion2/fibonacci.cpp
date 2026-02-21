f9061227c2d5ab438cf1be7a9823f083c17b326e
dileep yadav <dy695354612@gmail.com>
2026-02-21 18:15:01 +0530

    recursion

--- cplus program/.vscode/Recursion1/recursion2/fibonacci.cpp
+++ cplus program/.vscode/Recursion1/recursion2/fibonacci.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int fibo(int n){
     if(n==1 || n==2) return 1;
-    return fibo(n-1) + fibo(n-2);
+    int leftAns = fibo(n-1);
+    int rightAns = fibo(n-2);
+    return leftAns + rightAns; 
 }
 int main(){
     // 1 1 2 3 5 8 13 21 34 55 89.....
