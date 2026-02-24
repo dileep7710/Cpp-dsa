b25be8a79a237fc53ea2aaec644ad0402212f8ec
dileep yadav <dy695354612@gmail.com>
2026-02-24 23:08:17 +0530

    p js

--- .vscode/JavaScript/app.js
+++ .vscode/JavaScript/app.js
@@ -463,12 +463,23 @@
 // }
 // console.log(square(4));
 
-console.log("there!");
-let id = setInterval(() => {
-    console.log("Apna college");
-}, 2000);
+// console.log("there!");
+// let id = setInterval(() => {
+//     console.log("Apna college");
+// }, 2000);
 
-setTimeout(() => {
-    clearInterval(id);
-    console.log("clear interval ran");
-}, 10000);
+// setTimeout(() => {
+//     clearInterval(id);
+//     console.log("clear interval ran");
+// }, 10000);
+
+const arrayAverage = (arr) => {
+    let total = 0;
+    for(let number of arr) {
+        total += number;
+    }
+    return total / arr.length;
+};
+
+let arr = [1, 2, 3, 4, 5, 6];
+console.log(arrayAverage(arr));
