d516cf12d17395b5aeb8867b352b94e38806035a
dileep yadav <dy695354612@gmail.com>
2026-02-25 18:40:12 +0530

    js

--- .vscode/JavaScript/jv26.js
+++ .vscode/JavaScript/jv26.js
@@ -14,20 +14,37 @@
 //     console.log(el);
 // });
 
-let arr = [{
-    name: "aman",
-    marks:95
-},
-{
-    name:"shardha",
-    marks:94.4
-},
-{
-    name:"rajat",
-    maks:92
-},
-];
+// let students = [{
+//     name: "aman",
+//     marks:95
+// },
+// {
+//     name:"shardha",
+//     marks:94.4
+// },
+// {
+//     name:"rajat",
+//     maks:92
+// },
+// ]; 
 
-arr.forEach((student) =>{
-    console.log(student.name);
+// let gpa = students.map((el) => {
+//     return el.marks / 10;
+
+// });
+
+// arr.forEach((student) =>{
+//     console.log(student.name);
+// });
+
+// let num = [1, 2, 3, 4];
+
+// let double = num.map(function(el) {
+//     return el*el;
+// });
+
+
+let nums = [1, 2, 3, 4, 7, 8, 2, 9, 10, 12, 11];
+let ans = nums.filter((el) => {
+    return el % 2 == 0;  // even -> true, odd -> false
 });
