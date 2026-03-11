6a0e23837981392caaf2f772690c968851fcb17c
dileep yadav <dy695354612@gmail.com>
2026-03-11 22:00:15 +0530

    power logrithmic

--- cplus program/.vscode/Recursion1/sumFrom1toN.cpp
+++ cplus program/.vscode/Recursion1/sumFrom1toN.cpp
@@ -22,18 +22,38 @@
 //     cout<<sum(5);
 // }
 
+// #include<iostream>
+// using namespace std;
+// int power(int a, int b){
+//     if(b==0) return 1;
+//     return a*power(a,b-1);
+// }
+// int main(){
+//     int a;
+//     cout<<"Enter base : ";
+//     cin>>a;
+//     int b;
+//     cout<<"Enter exponent: ";
+//     cin>>b;
+//     cout<<a<< "raised to the power"<<b<<" is"<<power(a,b);
+// }
+
 #include<iostream>
 using namespace std;
-int power(int a, int b){
-    if(b==0) return 1;
-    return a*power(a,b-1);
+int pow(int x, int n){
+    if(n==1) return x;
+    if(n==0) return 1;
+
+    if(n%2==0){
+        int ans = pow(x,n/2);
+        return ans*ans; 
+    }
+
+    else {
+        int ans = pow(x,n/2);
+        return ans*ans*x;
+    }
 }
 int main(){
-    int a;
-    cout<<"Enter base : ";
-    cin>>a;
-    int b;
-    cout<<"Enter exponent: ";
-    cin>>b;
-    cout<<a<< "raised to the power"<<b<<" is"<<power(a,b);
+    cout<<pow(3,5);
 }
