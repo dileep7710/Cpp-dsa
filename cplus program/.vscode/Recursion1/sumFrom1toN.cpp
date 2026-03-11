d7922aec50e83cb4f5bc2ca0b64e86aab914aea6
dileep yadav <dy695354612@gmail.com>
2026-03-11 11:22:26 +0530

    recursion

--- cplus program/.vscode/Recursion1/sumFrom1toN.cpp
+++ cplus program/.vscode/Recursion1/sumFrom1toN.cpp
@@ -11,13 +11,29 @@
 //     sum1toN(0,10);
 // }
 
+// #include<iostream>
+// using namespace std;
+// int sum(int n){
+//     if(n==0) return 0;
+//     return n+sum(n-1);
+// }
+
+// int main(){
+//     cout<<sum(5);
+// }
+
 #include<iostream>
 using namespace std;
-int sum(int n){
-    if(n==0) return 0;
-    return n+sum(n-1);
+int power(int a, int b){
+    if(b==0) return 1;
+    return a*power(a,b-1);
 }
-
 int main(){
-    cout<<sum(5);
+    int a;
+    cout<<"Enter base : ";
+    cin>>a;
+    int b;
+    cout<<"Enter exponent: ";
+    cin>>b;
+    cout<<a<< "raised to the power"<<b<<" is"<<power(a,b);
 }
