4a0c4326aa0895a70bf77cd6d97a171203e29a97
dileep yadav <dy695354612@gmail.com>
2026-03-31 15:04:55 +0530

    js

--- .vscode/app(30).js
+++ .vscode/app(30).js
@@ -1,14 +1,32 @@
-function one(){
-    return 1;
-}
+// function one(){
+//     return 1;
+// }
 
-function two(){
-    return one() + one();
-}
+// function two(){
+//     return one() + one();
+// }
 
-function three() {
-    let ans = two() + one();
-    console.log(ans);
-}
+// function three() {
+//     let ans = two() + one();
+//     console.log(ans);
+// }
 
-three();
+// three();
+
+let a = 25;
+console.log(a);
+let b = 10;
+console.log(b);
+console.log(a+b);
+
+
+// synchrous nature
+
+setTimeout(() => {
+    console.log("apna college");
+}, 2000);
+setTimeout(() => {
+    console.log("hello world");
+}, 2000);
+
+console.log("hello....");
