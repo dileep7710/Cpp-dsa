7a345ee3ae4fe1e9cfdb402408a88c2ee2c93e0e
dileep yadav <dy695354612@gmail.com>
2026-04-01 08:01:13 +0530

    js

--- .vscode/app(30).js
+++ .vscode/app(30).js
@@ -22,38 +22,67 @@
 
 // // synchrous nature
 
-setTimeout(() => {
-    console.log("apna college");
-}, 2000);
-setTimeout(() => {
-    console.log("hello world");
-}, 2000);
+// setTimeout(() => {
+//     console.log("apna college");
+// }, 2000);
+// setTimeout(() => {
+//     console.log("hello world");
+// }, 2000);
 
-console.log("hello....");
+// console.log("hello....");
 
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
 
-function changeColor(color, delay, nextColorChange) {
-    setTimeout(() => {
-        h1.style.color = color;
-        if (nextColorChange) nextColorChange();
-    }, delay);
-}
+// function changeColor(color, delay, nextColorChange) {
+//     setTimeout(() => {
+//         h1.style.color = color;
+//         if (nextColorChange) nextColorChange();
+//     }, delay);
+// }
 
-changeColor("red", 1000, () => {
-    changeColor("orange",1000, () => {
-        changeColor("green",1000, () => {
-            changeColor("yellow",1000);
+// changeColor("red", 1000, () => {
+//     changeColor("orange",1000, () => {
+//         changeColor("green",1000, () => {
+//             changeColor("yellow",1000);
 
-        });
-    });
-});
+//         });
+//     });
+// });
 
 // callbacks nesting -> callback hell
 
 
 
+function savetoDb(data, success, failure) {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if(internetSpeed > 4) {
+        success();
+    } else {
+        failure();
+    }
+}
+
+ savetoDb("apna college", () => {
+    console.log("success : your data was saved :");
+    savetoDb("hello world", () => {
+        console.log("success2 : data2 saved");
+        savetoDb("shardha", ()=>{
+            console.log("success3: data3 saved");
+        },
+        () => {
+            console.log("failure3: weak connection");
+        }
+    );
+    },
+ () => {
+    console.log("failure2 : weak connection");
+ }
+);
+ }, 
+  () => {
+    console.log("failure : weak connection. data not saved");
+  });
 
   
 
