f190eed55b8114f13b516163316c1fc9432d169b
dileep yadav <dy695354612@gmail.com>
2026-04-02 11:21:51 +0530

    js

--- .vscode/app(30).js
+++ .vscode/app(30).js
@@ -31,15 +31,35 @@
 
 // console.log("hello....");
 
-// h1 = document.querySelector("h1");
+h1 = document.querySelector("h1");
 
 
-// function changeColor(color, delay, nextColorChange) {
-//     setTimeout(() => {
-//         h1.style.color = color;
-//         if (nextColorChange) nextColorChange();
-//     }, delay);
-// }
+function changeColor(color, delay) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        h1.style.color = color;
+        resolve("color changed!");
+   }, delay);
+ }); 
+   
+}
+
+changeColor("red", 1000)
+.then(() => {
+    console.log("red color was completed");
+    return changeColor("orange",1000);
+})
+.then(() => {
+    console.log("orange color was completed");
+    return changeColor("green", 1000);
+})
+.then(() => {
+    console.log("green color was completed");
+    return changeColor("blue", 1000);
+})
+.then(() => {
+    console.log("blue color was completed");
+});
 
 // changeColor("red", 1000, () => {
 //     changeColor("orange",1000, () => {
@@ -86,17 +106,30 @@
 
   
 
-function savetoDb(data) {
-    return new Promise((resolve, reject) => {
-        let internetSpeed = Math.floor(Math.random() * 10) +1;
-        if(internetSpeed >4){
-            resolve("success : data was saved");
-        } else {
-            reject("failure : weak connection");
-        }
-    })
-}
+// function savetoDb(data) {
+//     return new Promise((resolve, reject) => {
+//         let internetSpeed = Math.floor(Math.random() * 10) +1;
+//         if(internetSpeed >4){
+//             resolve("success : data was saved");
+//         } else {
+//             reject("failure : weak connection");
+//         }
+//     })
+// }
 
 
 
-  
+// savetoDb("apna college")
+//     .then((result) => {
+//         console.log("data1 saved");
+//         console.log("result of promise: ", result);
+//         return savetoDb("helloworld");
+//     })
+//     .then((result) => {
+//         console.log("data2 saved");
+//         console.log("result of promise: ", result);
+//     })
+//     .catch((error) => {
+//         console.log("promise was rejected");
+//         console.log(error);
+//     });
