c8203d191c84d253380f4ed758cc7fa156a96e81
dileep yadav <dy695354612@gmail.com>
2026-04-03 15:31:19 +0530

    js

--- .vscode/app(30).js
+++ .vscode/app(30).js
@@ -193,3 +193,18 @@ async function demo() {
 //      getNum();
 //      getNum();
 // }
+
+
+let url ="https://catfact.ninja/fact";
+
+fetch(url)
+ .then((res) => {
+    console.log(res);
+    return res.json();
+ })
+ .then((data) => {
+    console.log(data.fact);
+ })
+ .catch((err) => {
+    console.log("ERROR - ", err);
+ });
