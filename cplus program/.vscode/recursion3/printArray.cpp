0eb745566397c6ba8773b16819b4f357c4ba015c
Dileep Yadav <dy695354612@gmail.com>
2026-05-24 16:37:54 +0530

    print Array

--- cplus program/.vscode/recursion3/printArray.cpp
+++ cplus program/.vscode/recursion3/printArray.cpp
@@ -1,3 +1,23 @@
+// #include<iostream>
+// using namespace std;
+// void display(int arr[], int n, int idx){
+//     if(idx==n) return;
+//     cout<<arr[idx]<<" ";
+//     display(arr,n,idx+1);
+// }
+// int main(){
+//     int arr[] = {2,1,6,5,7,8,9,0,4};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     display(arr,n,0);
+// } 
+
+
+
+
+
+
+
+
 #include<iostream>
 using namespace std;
 void display(int arr[], int n, int idx){
@@ -6,7 +26,19 @@ void display(int arr[], int n, int idx){
     display(arr,n,idx+1);
 }
 int main(){
-    int arr[] = {2,1,6,5,7,8,9,0,4};
+    int arr[] = {2,1,6,3,9,0,2,7};
     int n = sizeof(arr)/sizeof(arr[0]);
     display(arr,n,0);
-} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
