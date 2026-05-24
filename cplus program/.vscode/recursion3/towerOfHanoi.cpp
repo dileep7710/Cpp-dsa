5c2d438129a4fcff265439b44753b5d0f7318f66
Dileep Yadav <dy695354612@gmail.com>
2026-05-24 14:38:27 +0530

    tower of hanoi

--- cplus program/.vscode/recursion3/towerOfHanoi.cpp
+++ cplus program/.vscode/recursion3/towerOfHanoi.cpp
@@ -1,12 +1,44 @@
+// #include<iostream>
+// using namespace std;
+// void hanoi(int n, char a, char b, char c){
+//     if(n==0) return;
+//     hanoi(n-1,a,c,b);
+//     cout<<a<<" -> "<<c<<endl;
+//     hanoi(n-1,b,a,c);
+// }
+// int main(){
+//     int n = 4;
+//     hanoi(n,'A','B','C');
+// }
+
+
+
+
+
+
 #include<iostream>
 using namespace std;
-void hanoi(int n, char a, char b, char c){
+void hanoi(int n,char a, char b, char c){
     if(n==0) return;
     hanoi(n-1,a,c,b);
     cout<<a<<" -> "<<c<<endl;
     hanoi(n-1,b,a,c);
+
 }
 int main(){
-    int n = 4;
+    int n = 3;
     hanoi(n,'A','B','C');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
