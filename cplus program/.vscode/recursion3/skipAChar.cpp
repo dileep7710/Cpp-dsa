d24b127ee52502aab9814b235047802fd2952b9f
Dileep Yadav <dy695354612@gmail.com>
2026-05-25 11:27:14 +0530

    skipAChar

--- cplus program/.vscode/recursion3/skipAChar.cpp
+++ cplus program/.vscode/recursion3/skipAChar.cpp
@@ -1,12 +1,32 @@
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// int main(){
+//     string str = "Raghav Garg";
+//     string s = "";
+//     for(int i=0;i<str.length();i++){
+//         if(str[i]!='a') s+= str[i];
+//     }
+//     cout<<s;
+// }
+
+
 #include<iostream>
 #include<string>
-using namespace std;
 
-int main(){
-    string str = "Raghav Garg";
-    string s = "";
-    for(int i=0;i<str.length();i++){
-        if(str[i]!='a') s+= str[i];
+using namespace std;
+void removeChar(string ans, string original, int idx){
+    if(idx==original.length()){
+        cout<<ans;
+        return;
     }
-    cout<<s;
+    char ch = original[idx];
+    if(ch=='p') removeChar(ans,original,idx+1);
+    else removeChar(ans+ch,original,idx+1);
+
+}
+int main(){
+    string str = "physics wallah";
+    removeChar("", str,0);
 }
