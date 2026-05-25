8d5a62045f28372e17eb923d3db71b5702cbeff0
Dileep Yadav <dy695354612@gmail.com>
2026-05-25 16:24:22 +0530

    node.js express

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
-console.dir(app);
+// console.dir(app);
 
 
 let port = 8080;
 
 app.listen(port, () => {
-    console.log('app is listening on port ${port}');
+    console.log(`app is listening on port ${port}`);
 } );
 
-app.use((req, res) => {
-    console.log("request received");
-    let  code = "<h1>Fruits</h> <ul><li>apple</li><li>orange</li></ul>"; 
-    res.send(code);
+// app.get("/", (req,res) =>{
+//     res.send("hello, i am root");
+// });
+
+// app.get("/", (req, res) => {
+//     res.send("hello, i am root");
+// });
+
+app.get("/:username/:id", (req, res) => {
+    let {username, id} = req.params;
+    let htmlstr = `welcome to the page of @${username}!` 
+    res.send(htmlstr);
+});
+
+app.get("/search", (req, res) =>{
+   let {q} = req.query;
+   if(!q) {
+    res.send("nothing searched");
+   }
+    res.send(`search results for query: ${q}`);
 });
