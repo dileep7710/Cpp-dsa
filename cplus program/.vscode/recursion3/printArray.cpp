473225e5ad93267356830cecdc7a358d75c9f174
Dileep Yadav <dy695354612@gmail.com>
2026-05-27 13:57:41 +0530

    merge  sort

--- cplus program/.vscode/recursion3/printArray.cpp
+++ cplus program/.vscode/recursion3/printArray.cpp
@@ -1,44 +1,14 @@
-// #include<iostream>
-// using namespace std;
-// void display(int arr[], int n, int idx){
-//     if(idx==n) return;
-//     cout<<arr[idx]<<" ";
-//     display(arr,n,idx+1);
-// }
-// int main(){
-//     int arr[] = {2,1,6,5,7,8,9,0,4};
-//     int n = sizeof(arr)/sizeof(arr[0]);
-//     display(arr,n,0);
-// } 
-
-
-
-
-
-
-
-
 #include<iostream>
 using namespace std;
+
 void display(int arr[], int n, int idx){
-    if(idx==n) return;
-    cout<<arr[idx]<<" ";
-    display(arr,n,idx+1);
+    if(idx == n) return;
+    cout << arr[idx] << " ";
+    display(arr, n, idx + 1);
 }
+
 int main(){
-    int arr[] = {2,1,6,3,9,0,2,7};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    display(arr,n,0);
+    int arr[] = {2, 1, 6, 3, 9, 0, 2, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    display(arr, n, 0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
