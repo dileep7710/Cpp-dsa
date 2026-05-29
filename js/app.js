30c8227858dc6e1f041681011a71f0e3e4055a9f
Dileep Yadav <dy695354612@gmail.com>
2026-05-29 11:52:40 +0530

    js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
-let pencilPrice = 10;
-let erasorPrice = 5;
-let output = "The total price is :" +( pencilPrice + erasorPrice) + "Rupees."
-console.log(output);
+// let pencilPrice = 10;
+// let erasorPrice = 5;
+// let output = "The total price is :" +( pencilPrice + erasorPrice) + "Rupees."
+// console.log(output);
+
+
+//  operators
+
+// let age = 18;
+// console.log(age >= 18);  // false 
+
+
+console.log("before my if statement");
+let age = 14;
+if(age >= 18){
+    console.log("you can vote");
+    console.log("you can drive");
+    let a = 5;
+    console.log(5 * a);
+}
+if(age < 18)  {
+    console.log("you cannot vote");
+}
+console.log("after my if statement");
