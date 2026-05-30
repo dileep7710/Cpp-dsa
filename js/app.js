d8815fdb9033eb6bf853be57794360d68745b518
Dileep Yadav <dy695354612@gmail.com>
2026-05-30 10:55:48 +0530

    js

--- js/app.js
+++ js/app.js
@@ -25,19 +25,19 @@
 
 
 // Traffic Light System 
-let color = "yellow";
+// let color = "yellow";
 
-if(color === "red"){
-    console.log("stop, Light color is red");
-}
+// if(color === "red"){
+//     console.log("stop, Light color is red");
+// }
 
- else if(color === "green, Light color is green"){
-    console.log("Go");
-}
+//  else if(color === "green, Light color is green"){
+//     console.log("Go");
+// }
 
-else if(color === "yellow"){
-    console.log("slow Down, light color is yellow  ");
-}
+// else if(color === "yellow"){
+//     console.log("slow Down, light color is yellow  ");
+// }
 
 // let marks = 65;
 
@@ -53,3 +53,15 @@ else if(color === "yellow"){
 // else if(marks <33){
 //     console.log("F");
 // }
+
+let size = 'XL';
+if(size === 'XL'){
+    console.log("size is 'XL', price is Rs.250");
+}
+else if(size =='L'){
+    console.log("size is 'L',price is Rs.200");
+}
+else if(size === 'M'){
+    console.log("size is 'M', price is Rs.100");
+}
+else if("size === 'S', price is Rs.50");
