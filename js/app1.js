f091154dacaf25a29ad6812268e866092ea73a74
Dileep Yadav <dy695354612@gmail.com>
2026-06-01 14:13:26 +0530

    javaScript

---
+++ js/app1.js
@@ -0,0 +1,18 @@
+// let msg = "   Hello   ";
+
+//  let password = prompt("set your password");
+//  let newPass = password.trim();
+//  console.log(newPass);
+
+// let name = "Apna college";
+
+// let msg = "ILoveCoding";
+
+let msg = "  hello ";
+let newMsg = msg.trim();
+console.log(newMsg);
+newMsg = newMsg.toUpperCase();
+console.log(newMsg);
+
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
