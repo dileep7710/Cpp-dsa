ac1d67c8adabbb53a5f948a865fc0d1c682244ab
Dileep Yadav <dy695354612@gmail.com>
2026-06-21 16:47:35 +0530

    even number

--- js/app1.js
+++ js/app1.js
@@ -39,9 +39,15 @@
 
 
 
-for(let i = 1; i<=15; i++){
-    if(i%2!=0){
-        console.log("It is odd numbers", i);
+// for(let i = 1; i<=15; i++){
+//     if(i%2!=0){
+//         console.log("It is odd numbers", i);
+//     }
+// }
+
+
+for(let i=1; i<=10; i++){
+    if(i%2==0){
+        console.log("It is even number", i); 
     }
 }
-
