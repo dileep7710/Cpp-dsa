aa4c12b154ff67068fd5389561dcb937db1d0e9f
Dileep Yadav <dy695354612@gmail.com>
2026-06-23 22:32:13 +0530

    objects

--- js/app1.js
+++ js/app1.js
@@ -216,6 +216,17 @@
 //         largest = arr[i];
 //     }
 //     console.log(largest);
-// }
+//
+
+let student = {
+    name: "shradha",
+    age: 23,
+    marks: 94.4,
+};
 
 
+const item = {
+    price : 100.99,
+    discount: 50,
+    color = ["red", "pink"]
+};
