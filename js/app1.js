a73ac9ee40010b5140c7585f4ccc44a796508dc2
Dileep Yadav <dy695354612@gmail.com>
2026-06-23 22:07:58 +0530

    home work

--- js/app1.js
+++ js/app1.js
@@ -140,38 +140,80 @@
 //     console.log(char);
 // }
 
-let todo = [];
+// let todo = [];
 
-let req = prompt("please enter your request");
-console.log(req);
+// let req = prompt("please enter your request");
+// console.log(req);
 
-while(true) {
-    if(req == "quit") {
-        console.log("quitting app");
-        break;
-    }
+// while(true) {
+//     if(req == "quit") {
+//         console.log("quitting app");
+//         break;
+//     }
 
-    if(req == "list"){
-        console.log("----------------");
-        for(let i=0; i<todo.length; i++){
-            console.log(i, todo[i]);
-        }
-        for(task of todo) {
-            console.log(task);
-        }
-        console.log("----------------");
-    } else if(req == "add"){
-        let task = prompt("please enter the task you want to add");
-        todo.push(task);
-        console.log("task added");
+//     if(req == "list"){
+//         console.log("----------------");
+//         for(let i=0; i<todo.length; i++){
+//             console.log(i, todo[i]);
+//         }
+//         for(task of todo) {
+//             console.log(task);
+//         }
+//         console.log("----------------");
+//     } else if(req == "add"){
+//         let task = prompt("please enter the task you want to add");
+//         todo.push(task);
+//         console.log("task added");
+//     }
+//     else if(req == "delete") {
+//         let idx = prompt("please enter the task index");
+//         todo.splice(idx, 1);
+//         console.log("task deleted");
+//     }
+//     else {
+//         console.log("wrong request");
+//     }
+//     req = prompt("please enter your request");
+// }
+
+
+// let arr = [1,2,3,4,5,2,3];
+// let num = 2;
+// for(let i = 0; i<arr.length; i++){
+//     if(arr[i] == num){
+//         arr.splice(i, 1);
+//     }
+
+// }
+// console.log(arr);
+
+// let number = 287152;
+// let sum = 0;
+// let copy = number;
+
+// while(copy > 0) {
+//     digit = copy % 10;
+//     sum+= digit;
+//     copy= Math.floor(copy/10);
+// }
+// console.log(sum);
+
+// let number = 287152;
+// let count = 0;
+
+// let copy = number;
+// while(number > 0){
+//     count++;
+//     number = Math.floor(number/10);
+// }
+// console.log(count);
+
+let arr = [2, 5, 10, 4, 2, 7, 1, 9];
+let largest = 0;
+
+for(let i=0; i<=arr.length; i++){
+    if(largest < arr[i]){
+        largest = arr[i];
     }
-    else if(req == "delete") {
-        let idx = prompt("please enter the task index");
-        todo.splice(idx, 1);
-        console.log("task deleted");
-    }
-    else {
-        console.log("wrong request");
-    }
-    req = prompt("please enter your request");
+    console.log(largest);
 }
