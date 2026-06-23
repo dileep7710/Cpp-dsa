3d5e2f301f63ab06add995f6529218a00daf2ac7
Dileep Yadav <dy695354612@gmail.com>
2026-06-23 17:14:26 +0530

    Todo

--- js/app1.js
+++ js/app1.js
@@ -130,12 +130,48 @@
 //     }
 // }
 
-let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+// let fruits = ["mango", "apple", "banana", "litchi", "orange"];
 
-for(fruit of fruits) {
-    console.log(fruit);
-}
+// for(fruit of fruits) {
+//     console.log(fruit);
+// }
 
-for(char of "apnacollege"){
-    console.log(char);
+// for(char of "apnacollege"){
+//     console.log(char);
+// }
+
+let todo = [];
+
+let req = prompt("please enter your request");
+console.log(req);
+
+while(true) {
+    if(req == "quit") {
+        console.log("quitting app");
+        break;
+    }
+
+    if(req == "list"){
+        console.log("----------------");
+        for(let i=0; i<todo.length; i++){
+            console.log(i, todo[i]);
+        }
+        for(task of todo) {
+            console.log(task);
+        }
+        console.log("----------------");
+    } else if(req == "add"){
+        let task = prompt("please enter the task you want to add");
+        todo.push(task);
+        console.log("task added");
+    }
+    else if(req == "delete") {
+        let idx = prompt("please enter the task index");
+        todo.splice(idx, 1);
+        console.log("task deleted");
+    }
+    else {
+        console.log("wrong request");
+    }
+    req = prompt("please enter your request");
 }
