6acbb16fe56788e4ae75218d393e74ce1a5e570a
Dileep Yadav <dy695354612@gmail.com>
2026-06-23 22:09:19 +0530

    home work

--- js/app1.js
+++ js/app1.js
@@ -208,12 +208,14 @@
 // }
 // console.log(count);
 
-let arr = [2, 5, 10, 4, 2, 7, 1, 9];
-let largest = 0;
+// let arr = [2, 5, 10, 4, 2, 7, 1, 9];
+// let largest = 0;
 
-for(let i=0; i<=arr.length; i++){
-    if(largest < arr[i]){
-        largest = arr[i];
-    }
-    console.log(largest);
-}
+// for(let i=0; i<=arr.length; i++){
+//     if(largest < arr[i]){
+//         largest = arr[i];
+//     }
+//     console.log(largest);
+// }
+
+
