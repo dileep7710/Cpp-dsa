2ade91c1535ecf822520cb340c84c3ebc8b2bfa0
Dileep Yadav <dy695354612@gmail.com>
2026-06-23 11:03:10 +0530

    loop

--- js/app1.js
+++ js/app1.js
@@ -121,11 +121,21 @@
 // }
 
 
-let student = [["aman", 95], ["shradha", 94.4], ["karan", 100]];
+// let student = [["aman", 95], ["shradha", 94.4], ["karan", 100]];
 
-for(let i=0; i<student.length; i++){
-    console.log(`info of # ${i+1}`);
-    for(let j=0; j<student[i].length; j++){
-        console.log(student[i][j]);
-    }
+// for(let i=0; i<student.length; i++){
+//     console.log(`info of # ${i+1}`);
+//     for(let j=0; j<student[i].length; j++){
+//         console.log(student[i][j]);
+//     }
+// }
+
+let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+
+for(fruit of fruits) {
+    console.log(fruit);
+}
+
+for(char of "apnacollege"){
+    console.log(char);
 }
