ee2ae9c95c9123f6bf4786fe645346f9560eaf31
Dileep Yadav <dy695354612@gmail.com>
2026-06-27 16:50:25 +0530

    guessing game

--- js/app1.js
+++ js/app1.js
@@ -246,17 +246,47 @@
 //     city: "Delhi"
 // };
 
-const classInfo = {
-    aman:{
-        grade: "A+",
-        city: "Delhi"
-    },
-    shradha:{
-        grade: "A",
-        city: "Mumbai"
-    },
-    karan:{
-        grade: "O",
-        city: "Kolkatta"
-    },
-}; 
+// const classInfo = {
+//     aman:{
+//         grade: "A+",
+//         city: "Delhi"
+//     },
+//     shradha:{
+//         grade: "A",
+//         city: "Mumbai"
+//     },
+//     karan:{
+//         grade: "O",
+//         city: "Kolkatta"
+//     },
+// }; 
+
+
+const max = prompt("Enter the max number");
+
+
+const random = Math.floor(Math.random()*max) + 1;
+console.log(random);
+
+let guess = prompt("guess the number");
+
+while(true){
+    if(guess == "quit"){
+        console.log("user quit");
+        break; 
+    }
+
+    if(guess == random){
+        console.log("you are right! congrats!!");
+        break;
+    }
+    else if(guess < random){
+        guess = prompt("your guess was too small. please try again");
+    }
+    else {
+        guess = prompt("your guess was too large. please try again"); 
+    } 
+    // else {
+    //     guess = prompt("your guess was wrong. please try again");
+    // }
+}
