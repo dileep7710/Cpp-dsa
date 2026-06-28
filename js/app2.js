5f01f7f0e9751978a2404b547a0827bb302c0d56
Dileep Yadav <dy695354612@gmail.com>
2026-06-28 08:01:06 +0530

    function

---
+++ js/app2.js
@@ -0,0 +1,43 @@
+// function hello(){
+//     console.log("hello");
+// }
+
+
+
+// function printName(){
+//     console.log("Apna college");
+//     console.log("Apna college");
+// }
+
+// function print1to5() {
+//     for(let i=1; i<=5; i++){
+//         console.log(i);
+//     }
+// }
+
+
+// function isAdult(){
+//     let age = 17 ;
+//     if(age >=18){
+//         console.log("Adult");  
+//     }
+//     else {
+//         console.log("not adult"); 
+//     }
+// }
+
+// isAdult();
+
+
+// function printPoem(){
+//     console.log("Twinkle Twinkle little star");
+// }
+
+// printPoem();
+
+function rolldice(){
+    let random = Math.floor(Math.random()*6)+1;
+    console.log(random);
+}
+
+rolldice();
